c25592e093c586b18c4939ef88bfbc32f691b003
Jialu-Huang <47579618+Jialu-Huang@users.noreply.github.com>
2019-11-09 22:27:53 -0800

    Update Data calculation.m

--- Data calculation.m
+++ Data calculation.m
@@ -2,10 +2,10 @@ clear
 close
 clc
 
-%% Read in csv file
+% Read in csv file
 data = csvread('TG_data.csv',1,0);
 
-%% Plot certain frame
+% Plot certain frame
 frame = 1;
 time = data(frame, 1);
 for line = 1:17
@@ -17,7 +17,7 @@ pcolor(temp); axis square
 title( ['Frame: ', num2str(frame), '. At ', num2str(time), ' s'] )
 colorbar
 
-%% Average out all frames (0.01s)
+% Average out all frames (0.01s)
 time_ave = data(50, 1);
 for p = 0:16
     temp_ave(p+1,:) = sum(data((1+50*p):50+50*p, 2:end))./50; % 1:50, 51:100, 101:150, ...
@@ -27,7 +27,7 @@ pcolor(temp_ave); axis square
 title(['Average out all 50 frames. ', 'In ', num2str(time_ave), ' s'])
 colorbar
 
-%% Add filter to calaulate area rate
+% Add filter to calaulate area rate
 filter = 41.3; %threshold
 temp_ave(temp_ave<filter)=0;
 temp_ave(temp_ave>=filter)=1;
